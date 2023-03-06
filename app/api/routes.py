2a194de68bb5fbc37cf959810df707c9daafe4c0
corey.nadeau <djcjn16@hotmail.com>
2023-03-05 21:46:53 -0600

    second commit

--- app/api/routes.py
+++ app/api/routes.py
@@ -1,8 +1,69 @@
 from flask import Blueprint, request, jsonify, render_template
 from helpers import token_required
-from models import db, User, Contact, contact_schema, contacts_schema
+from models import db, User, Car, car_schema, cars_schema
 
 api = Blueprint('api', __name__, url_prefix='/api')
 
-@api.route('/carinventory')
-def getcars():
+@api.route('/getdata')
+def getdata():
+    return {'yee':'haw'}
+
+@api.route('/cars', methods = ['POST'])
+@token_required
+def create_car(current_user_token):
+    make = request.json['make']
+    model = request.json['model']
+    year = request.json['year']
+    location = request.json['location']
+    user_token = current_user_token.token
+
+    print(f'BIG TESTER: {current_user_token.token}')
+
+    car = Car(make, model, year, location, user_token= user_token)
+
+    db.session.add(car)
+    db.session.commit()
+
+    response = car_schema.dump(car)
+    return jsonify(response)
+
+@api.route('/cars', methods = ['GET'])
+@token_required
+def get_car(current_user_token):
+    a_user = current_user_token.token
+    cars = Car.query.filter_by(user_token = a_user).all()
+    response = cars_schema.dump(cars)
+    return jsonify(response)
+
+@api.route('/cars/<id>', methods = ['GET'])
+@token_required
+def get_single_car(current_user_token, id):
+    car = Car.query.get(id)
+    response = car_schema.dump(car)
+    return jsonify(response)
+    
+
+
+@api.route('/cars/<id>', methods = ['POST','PUT'])
+@token_required
+def update_car(current_user_token, id):
+    cars = Car.query.get(id)
+    cars.make = request.json["make"]
+    cars.model = request.json['model']
+    cars.year = request.json['year']
+    cars.location = request.json['location']
+    cars.user_token = current_user_token.token
+
+    db.session.commit()
+    response = car_schema.dump(car)
+    return jsonify(response)
+
+
+@api.route('/cars/<id>', methods = ['DELETE'])
+@token_required
+def delete_car(current_user_token, id):
+    contact = Car.query.get(id)
+    db.session.delete(car)
+    db.session.commit()
+    response = car_schema.dump(car)
+    return jsonify(response)
--- config.py
+++ config.py
@@ -1,12 +1,13 @@
-import os
+import os 
 
-baseline = os.path.abspath(os.path.dirname(__file__))
+from dotenv import load_dotenv
+basedir = os.path.abspath(os.path.dirname(__file__))
+load_dotenv(os.path.join(basedir, '.env'))
 
 class Config():
+    
     FLASK_APP = os.getenv('FLASK_APP')
     FLASK_ENV = os.getenv('FLASK_ENV')
-    
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'You will never get it'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'error'
+    SQLALCHEMY_DATABASE_URI=os.environ.get('DATABASE_URI') or 'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_TRACK_NOTIFICATIONS = False
